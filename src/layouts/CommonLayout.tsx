a062597ef02f2a6ea7ef4d6645fdc71f74e0f9d3
Soomin <soomin3121@gmail.com>
2025-02-18 11:59:34 +0900

    style :업로드 페이지 모바일 세로 반응형 추가

--- src/layouts/CommonLayout.tsx
+++ src/layouts/CommonLayout.tsx
@@ -5,7 +5,7 @@ export default function CommonLayout() {
 	return (
 		<>
 			<div className='mx-auto min-h-screen min-w-[360px] max-w-[767px] bg-backColor'>
-				<Toast/>
+				<Toast />
 				<Outlet />
 			</div>
 		</>
--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -103,61 +103,59 @@ const UploadPage = () => {
 	console.log(fileList);
 
 	return (
-		<>
-			<div className='mx-default'>
-				<header className='pt-5'>
-					<div className='pb-2'>
-						<BackButton />
-					</div>
-					<h1 className='pb-1 text-xl font-bold'>{postId ? '게시글 수정' : '게시글 생성'}</h1>
-				</header>
-				<main className='flex justify-center'>
-					<div className='flex min-w-[300px] flex-col items-end'>
-						<div className='flex flex-col items-end'>
-							<label
-								htmlFor='file'
-								className='mb-3 flex w-5 cursor-pointer justify-center rounded-sm bg-mainColor duration-300 hover:bg-[#629ada] active:scale-95'
-							>
-								<BiPlus color='white' size={20} />
-							</label>
-							<input
-								type='file'
-								name='file'
-								id='file'
-								multiple
-								accept='image/* ,video/*'
-								className='hidden'
-								onFocus={(e) => (e.currentTarget.value = '')}
-								onChange={handleSaveFiles}
-								required
-							/>
-							<div className='h-[304px] w-[300px] rounded-default border-2 border-mainColor bg-white'>
-								<UploadCarousel slides={fileList} />
-							</div>
-							<div className='mb-2 mt-1 text-xs text-subText'>
-								* 사진 최소 1장, 최대 5장 (장당 5MB 제한) / 동영상 1개까지 첨부 가능 (10MB 제한)
-							</div>
+		<div className='mx-default flex min-h-[100svh] flex-col'>
+			<header className='pt-5'>
+				<div className='pb-2'>
+					<BackButton />
+				</div>
+				<h1 className='pb-1 text-xl font-bold'>{postId ? '게시글 수정' : '게시글 생성'}</h1>
+			</header>
+			<main className='flex h-auto w-full grow flex-col items-center pb-10'>
+				<div className='flex h-auto min-w-[300px] grow flex-col items-center justify-around'>
+					<div className='flex flex-col items-end'>
+						<label
+							htmlFor='file'
+							className='hover:bg-hoverColor mb-3 flex w-5 cursor-pointer justify-center rounded-sm bg-mainColor duration-300 active:scale-95'
+						>
+							<BiPlus color='white' size={20} />
+						</label>
+						<input
+							type='file'
+							name='file'
+							id='file'
+							multiple
+							accept='image/* ,video/*'
+							className='hidden'
+							onFocus={(e) => (e.currentTarget.value = '')}
+							onChange={handleSaveFiles}
+							required
+						/>
+						<div className='h-auto w-[300px] rounded-default border-2 border-mainColor bg-white'>
+							<UploadCarousel slides={fileList} />
 						</div>
-						<div className='relative inline-block'>
-							<textarea
-								id='description'
-								name='description'
-								maxLength={150}
-								minLength={0}
-								wrap='hard'
-								onChange={handleTextCount}
-								placeholder='오늘의 아우라를 표현해보세요 ✨'
-								className='mb-9 h-28 w-[300px] resize-none rounded-default p-2 text-base focus:border-[#9DC6F5] focus:outline-none focus:ring-2 focus:ring-[#9DC6F5]'
-							></textarea>
-							<div className='absolute bottom-11 right-2 text-sm text-subText'>
-								{maxLength - text.length} / {maxLength}
-							</div>
+						<div className='my-auto mb-auto text-xs text-subText'>
+							* 사진 최소 1장, 최대 5장 (장당 5MB 제한) / 동영상 1개까지 첨부 가능 (10MB 제한)
 						</div>
-						<Button children={'업로드'} onClick={handleFileUpload}></Button>
 					</div>
-				</main>
-			</div>
-		</>
+					<div className='relative flex h-full grow flex-col pt-4'>
+						<textarea
+							id='description'
+							name='description'
+							maxLength={150}
+							minLength={0}
+							wrap='hard'
+							onChange={handleTextCount}
+							placeholder='오늘의 아우라를 표현해보세요 ✨'
+							className='focus:border-hoverColor mb-9 h-full min-h-28 w-[300px] grow resize-none rounded-default p-2 text-base focus:outline-none focus:ring-2 focus:ring-subColor'
+						/>
+						<div className='absolute bottom-11 right-2 text-sm text-subText'>
+							{maxLength - text.length} / {maxLength}
+						</div>
+					</div>
+					<Button children={'업로드'} onClick={handleFileUpload}></Button>
+				</div>
+			</main>
+		</div>
 	);
 };
 export default UploadPage;
